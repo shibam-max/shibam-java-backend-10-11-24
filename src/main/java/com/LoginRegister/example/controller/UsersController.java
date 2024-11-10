23d0eaa74df00108be35e67216746ace54fbeed0
shibam_samaddar <shibamsamaddar1999@gmail.com>
2024-11-10 14:54:13 +0530

    backend changes

---
+++ src/main/java/com/LoginRegister/example/controller/UsersController.java
@@ -0,0 +1,30 @@
+package com.LoginRegister.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.LoginRegister.example.entity.Users;
+import com.LoginRegister.example.requests.LoginRequest;
+import com.LoginRegister.example.service.UserService;
+
+@RestController
+public class UsersController {
+
+	@Autowired
+	UserService userService;
+	
+	@PostMapping("/addUser")
+	@CrossOrigin(origins = "http://localhost:3000")
+	public Users addUser(@RequestBody Users user) {
+		return userService.addUser(user);
+	}
+	
+	@PostMapping("/loginUser")
+	@CrossOrigin(origins = "http://localhost:3000")
+	public Boolean loginUser(@RequestBody LoginRequest loginRequest) {
+		return userService.loginUser(loginRequest);
+	}
+}
